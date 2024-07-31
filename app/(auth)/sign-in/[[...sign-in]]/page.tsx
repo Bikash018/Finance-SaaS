50ba4a1e321677593614bf79b1d3580c6664274f
Bikash Prasad <bikashprasad101@gmail.com>
2024-07-31 23:19:31 +0530

    clerk authentication

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,19 @@
+import { Loader2 } from "lucide-react";
+import { SignIn ,ClerkLoaded,ClerkLoading } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+        <ClerkLoaded>
+        <SignIn />
+        </ClerkLoaded>
+        <ClerkLoading>
+            <div className="w-full flex justify-center">
+            <Loader2 className="animate-spin text-muted-foreground text-center"/>
+            </div>
+        </ClerkLoading>
+      </div>
+    </div>
+  );
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+const dashboard = ()=>{
+    return (
+        <p>This is protected route</p>
+    )
+}
+
+export default dashboard 
