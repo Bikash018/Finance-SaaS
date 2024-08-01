5882254c1b190a2b040b93ba9e5c6fd0b19d5ed6
Bikash Prasad <bikashprasad101@gmail.com>
2024-08-01 07:28:58 +0530

    hono js integrated

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,21 @@
-import { SignUp } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+<div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+  <ClerkLoaded>
+  <SignUp />
+  </ClerkLoaded>
+  <ClerkLoading>
+      <div className="w-full flex justify-center">
+      <Loader2 className="animate-spin text-muted-foreground text-center"/>
+      </div>
+  </ClerkLoading>
+</div>
+</div>
+  )
 }
+
+
