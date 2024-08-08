ebcb321405527e3d67d2e817df9e08d75fc5b341
Bikash Prasad <yaduvickey@gmail.com>
2024-08-08 07:43:08 +0530

    Schema updated and building apis

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,24 +1,29 @@
 import { Hono } from 'hono'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { handle } from 'hono/vercel'
+import accounts from "./accounts"
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', clerkMiddleware(), (c) => {
 
-  const auth = getAuth(c)
+app.route("/accounts", accounts)
 
-  if (!auth?.userId) {
-    return c.json({
-      message: 'You are not logged in.'
-    })
-  }
-  return c.json({
-    message: 'Hello Next.js!',
-  })
-})
+
+// app.get('/hello', clerkMiddleware(), (c) => {
+
+//   const auth = getAuth(c)
+
+//   if (!auth?.userId) {
+//     return c.json({
+//       message: 'You are not logged in.'
+//     })
+//   }
+//   return c.json({
+//     message: 'Hello Next.js!',
+//   })
+// })
 
 export const GET = handle(app)
 export const POST = handle(app)
