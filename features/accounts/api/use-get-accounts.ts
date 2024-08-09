25e5d93cb0982de3e81af5030f615a2f0387ed6d
Bikash Prasad <yaduvickey@gmail.com>
2024-08-09 07:56:16 +0530

    using react query

---
+++ features/accounts/api/use-get-accounts.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+
+import {client} from "@/lib/hono"
+
+
+export const userGetAccounts = ()=>{
+    const query = useQuery({
+        queryKey: ['posts'],
+        queryFn: async () => {
+            const response = await client.api.accounts.$get()
+            if(!response.ok){
+                throw new Error("Failed to fetch Response")
+            }
+
+            const {data} = await response.json()
+            return data
+
+        }
+    })
+}
