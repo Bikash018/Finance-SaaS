8955df7cd32170ac0751458fe8c4cdc516f82c0f
Bikash Prasad <yaduvickey@gmail.com>
2024-08-16 08:05:40 +0530

    dable added

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,7 +1,45 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
+import { Plus } from "lucide-react"
+import { columns, Payment } from "./columns"
+import { DataTable } from "@/components/data-table"
+
+
+
+  const data :Payment[]  = [
+    {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+  ]
 const AccountsPage = ()=>{
+    const newAccount = useNewAccount()
     return(
-        <div>
-            <p>Accounts Page</p>
+        <div className="max-w-2xl mx-auto mt-2">
+            <Card className="flex flex-col p-2">
+                <CardHeader>
+                    <CardTitle>Add Account</CardTitle>
+                 
+                </CardHeader>
+                <Button onClick={newAccount.onOpen}>
+                    <Plus className=" size-4 mr-2"/>
+                    Add New
+                </Button>
+                <CardContent className="w-full mt-2">
+                <DataTable columns={columns} data={data} />
+                </CardContent>
+                </Card>
         </div>
     )
 }
