06791a3ecf7f8714f083c31b9f62597f6167ed2b
Bikash Prasad <yaduvickey@gmail.com>
2024-09-24 07:47:46 +0530

    import csv file upload

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -16,18 +16,57 @@ import { DataTable } from "@/components/data-table"
 
 import { useBulkDeleteCategories } from "@/features/transactions/api/use-bulk-delete-transactions"
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions"
+import { useState } from "react"
+import { UploadButton } from "./upload-button"
+
+
+
+enum VARIANTS {
+    // eslint-disable-next-line no-unused-vars
+    LIST = 'LIST', IMPORT = 'IMPORT'
+  }
+  
+  const INITIAL_IMPORT_RESULTS = {
+    data: [],
+    errors: [],
+    meta: {}, // will be used by a package
+  };
 
 
 
 
 const TransactionsPage = ()=>{
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
     const newTransactions = useNewTransactions()
     const deleteTransactions = useBulkDeleteCategories()
     const transactionsQuery = useGetTransactions() 
     const transactions = transactionsQuery.data || []
 
+    const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+      };
+      const onCancelImport = () => {
+        setImportResults(INITIAL_IMPORT_RESULTS);
+        setVariant(VARIANTS.LIST);
+      };
+
     const isDisabled = 
     transactionsQuery.isLoading || deleteTransactions.isPending
+
+    if (variant === VARIANTS.IMPORT) {
+        return (
+          <>
+            {/* <AccountDialog /> */}
+            {/* <ImportCard
+              data={importResults.data}
+              onCancel={onCancelImport}
+              onSubmit={onSubmitImport}
+            /> */}
+          </>
+        );
+      }
     return(
         <div className="max-w-2xl mx-auto mt-2">
             <Card className="flex flex-col p-2">
@@ -39,6 +78,7 @@ const TransactionsPage = ()=>{
                     <Plus className=" size-4 mr-2"/>
                     Add New
                 </Button>
+                <UploadButton onUpload={onUpload} />
                 <CardContent className="w-full mt-2">
                 <DataTable columns={columns} data={transactions} onDelete={(row)=> {
                     const ids = row.map((r) => r.original.id)
